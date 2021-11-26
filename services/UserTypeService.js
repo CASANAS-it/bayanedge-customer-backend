bfb2bd18e9077a4e04c86a69c4860209d644acf5
rmarquez1116 <ronald.marquez111694@gmail.com>
2021-11-26 21:12:57 +0800

    Revert "init commit"
    This reverts commit 7f8930d682296218e67b042dd2c238af4ee18620.

--- services/UserTypeService.js
+++
@@ -1,9 +0,0 @@
-import UserTypeModel from '../models/UserTypeModel'
-
-const userTypeService = {
-  getAll: async () => {
-    return await UserTypeModel.getAll()
-  }
-}
-
-export default userTypeService
