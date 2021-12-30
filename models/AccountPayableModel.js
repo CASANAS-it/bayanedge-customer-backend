d7378a64c12c0628dee7fc23f8a6ec61f461a190
rmarquez1116 <ronald.marquez111694@gmail.com>
2021-12-30 23:59:59 +0800

    accounts payable delete

--- models/AccountPayableModel.js
+++ models/AccountPayableModel.js
@@ -199,7 +199,7 @@ const customModel = {
     return user
   },
   delete: async (params) => {
-    const user = await customModel.model.findOneAndUpdate({ id: params.id }, {
+    const user = await customModel.model.findOneAndUpdate({ id: params.transaction_id }, {
       is_active: false,
       modified_by: params.admin_id,
       modified_date: new Date(),
