5d9d76d20e1d3b353661dce3d38ab47b552d8ec8
rmarquez1116 <ronald.marquez111694@gmail.com>
2022-02-11 20:29:17 +0800

    microsavings pay

--- services/LoansPayableService.js
+++ services/LoansPayableService.js
@@ -108,6 +108,8 @@ const loansPayableService = {
     ms.total = params.microsavings;
     ms.display_id = "MS" + ap.display_id.substring(2);
     ms.details = current;
+    ms.details.name = "Microsavings"
+    ms.details.description = "Microsavings"
     ms.type_id = TransType.MICROSAVINGS;
     ms.flow_type_id = FlowType.OUTFLOW
     ms.is_posted = postToCashJournal;
@@ -163,13 +165,13 @@ const loansPayableService = {
     await CashJournalModel.create(cashJournal)
 
     var ms = JSON.parse(JSON.stringify(params));
-    var details = JSON.parse(JSON.stringify(current))
-    details.details.name= "Microsavings"
-    details.details.description= "Microsavings"
+
     ms.reference_id = current.transaction_id;
     ms.total = params.microsavings;
     ms.display_id = "MS" + ap.display_id.substring(2);
-    ms.details = details;
+    ms.details = current;
+    ms.details.name = "Microsavings"
+    ms.details.description = "Microsavings"
     ms.type_id = TransType.MICROSAVINGS;
     ms.is_posted = postToCashJournal
     ms.is_beginning = true
