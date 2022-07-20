e8b44a2b69a2f21173eebf63a3ef3e661286bda6
rmarquez1116 <ronald.marquez111694@gmail.com>
2022-07-20 17:37:07 +0800

    training instance

--- properties.js
+++ properties.js
@@ -3,10 +3,10 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 module.exports = {
-  butler_db_dbUrl: (process.env.DB_HOST || '114.198.129.250:27017/bayan_edge_db'),
-  butler_db_string: "mongodb://moneyflowAdmin:*m0n3y_flow001!DB@114.198.129.250:27017/bayan_edge_db?readPreference=primary&directConnection=true&ssl=false",
+  butler_db_dbUrl: (process.env.DB_HOST || '114.198.129.250:27017/bayan_edge_db_training'),
+  butler_db_string: "mongodb://moneyflowAdmin:*m0n3y_flow001!DB@114.198.129.250:27017/bayan_edge_db_training?readPreference=primary&directConnection=true&ssl=false",
   publicPath: '../client/build',
-  port: process.env.NODE_PORT || 6110,
+  port: process.env.NODE_PORT || 7110,
   tokenSecret: 'B@y@an3Dge!!M()n3yFl()w',
   api: process.env.NODE_API != null ? process.env.NODE_API : '/api',
  
