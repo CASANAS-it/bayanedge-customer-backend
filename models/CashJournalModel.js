b7fab2cff7a84289c8dfa6ff0b8418246b6c76c8
rmarquez1116 <ronald.marquez111694@gmail.com>
2022-08-01 00:15:38 +0800

    delete cash flow

--- models/CashJournalModel.js
+++ models/CashJournalModel.js
@@ -445,7 +445,7 @@ const customModel = {
     return user
   },
   delete: async (params) => {
-    const user = await customModel.getModel().findOneAndUpdate({ transaction_id: params.transaction_id }, {
+    const user = await customModel.getModel().findOneAndUpdate({ transaction_id: params.id }, {
       is_active: false,
       modified_by: params.admin_id,
       modified_date: new Date(),
