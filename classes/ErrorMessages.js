35c67df919ca43dddd99eaaa282ba89d9627cb7c
rmarquez1116 <ronald.marquez111694@gmail.com>
2022-11-25 11:11:33 +0800

    no message

--- classes/ErrorMessages.js
+++ classes/ErrorMessages.js
@@ -54,7 +54,7 @@ const ErrorMessages = Object.freeze({
   
   TRANSACTION_DELETE_ERROR: { status: 400, message: 'Tranasaction Completed, cannot delete the transaction' },
   
-  DUPLICATE_ENTRY: { status: 400, message: 'Name already exists', code: 601 },
+  DUPLICATE_ENTRY: { status: 200, message: 'Name already exists', code: 601 },
   DUPLICATE_REF: { status: 200, message: 'REF already exists', code: 602 },
   NO_DATA: { status: 200, message: 'No data found.', code: 603 },
   
