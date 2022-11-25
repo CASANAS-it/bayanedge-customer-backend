ac875b9c5bb3e462949f20f251c87fea4185644d
rmarquez1116 <ronald.marquez111694@gmail.com>
2022-11-25 11:25:54 +0800

    no message

--- properties.js
+++ properties.js
@@ -6,7 +6,7 @@ module.exports = {
   butler_db_dbUrl: (process.env.DB_HOST || '114.198.129.250:27017/bayan_edge_db_training'),
   butler_db_string: "mongodb://moneyflowAdmin:*m0n3y_flow001!DB@114.198.129.250:27017/bayan_edge_db_training?readPreference=primary&directConnection=true&ssl=false",
   publicPath: '../client/build',
-  port: process.env.NODE_PORT || 6110,
+  port: process.env.NODE_PORT || 7110,
   tokenSecret: 'B@y@an3Dge!!M()n3yFl()w',
   api: process.env.NODE_API != null ? process.env.NODE_API : '/api',
  
